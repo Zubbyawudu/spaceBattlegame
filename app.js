f69a5c0f02a344f6ee53e8e1d037f6f689079ae7
Zubairu Awudu <zubairuawudu123@gmail.com>
2025-01-04 23:57:44 -0500

    Space Battle

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+const humanAircraft = document.querySelector(".humanship");
+const alienAircraft = document.querySelector(".alienspaceships");
+const startGameIcon = document.querySelector(".startgameicon");
+const header = document.querySelector("h1");
+const headerStart = document.querySelector(".spaceh2");
+const startbtn = document.querySelector("button");
+const gameHistory = document.querySelector("#gamecomments");
+
+
+
 class ship{
   constructor(name, hull , firepower, accuracy){
     this.name = name;
@@ -7,3 +17,4 @@ class ship{
 
   }
 }
+
